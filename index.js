9aeb28fc6beb3a23382b4583fa617d95e7489207
GeanSousa28 <geandriossousadasilva09@gmail.com>
2026-04-28 10:20:57 -0300

    Enhance bot connection handling and pairing code
    Added connection update handling and pairing code generation.

--- index.js
+++ index.js
@@ -4,11 +4,27 @@ async function startBot() {
   const { state, saveCreds } = await useMultiFileAuthState("auth");
 
   const sock = makeWASocket({
-    auth: state,
-    printQRInTerminal: true
+    auth: state
   });
 
   sock.ev.on("creds.update", saveCreds);
+
+  sock.ev.on("connection.update", async ({ connection }) => {
+    if (connection === "open") {
+      console.log("✅ Bot conectado!");
+    }
+
+    if (connection === "close") {
+      startBot();
+    }
+  });
+
+  // 🔑 GERAR CÓDIGO DE CONEXÃO
+  if (!sock.authState.creds.registered) {
+    const phone = "5598981666909"; //
+    const code = await sock.requestPairingCode(phone);
+    console.log("🔗 Código de conexão:", code);
+  }
 
   sock.ev.on("messages.upsert", async ({ messages }) => {
     const msg = messages[0];
